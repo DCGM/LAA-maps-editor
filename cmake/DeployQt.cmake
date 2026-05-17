5668287e0836fba69fbd2de5927d780cea0709e4
Jozef Mlich <jozef.mlich@greycortex.com>
2026-05-17 12:06:54 +0200

    Update for Qt6

--- cmake/DeployQt.cmake
+++ cmake/DeployQt.cmake
@@ -20,11 +20,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-find_package(Qt5Core REQUIRED)
+find_package(Qt6Core REQUIRED)
 
-# Retrieve the absolute path to qmake and then use that path to find
-# the windeployqt and macdeployqt binaries
-get_target_property(_qmake_executable Qt5::qmake IMPORTED_LOCATION)
+get_target_property(_qmake_executable Qt6::qmake IMPORTED_LOCATION)
 get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
 
 find_program(WINDEPLOYQT_EXECUTABLE windeployqt HINTS "${_qt_bin_dir}")
@@ -37,29 +35,16 @@ if(APPLE AND NOT MACDEPLOYQT_EXECUTABLE)
     message(FATAL_ERROR "macdeployqt not found")
 endif()
 
-# Add commands that copy the required Qt files to the same directory as the
-# target after being built as well as including them in final installation
 function(windeployqt target)
-
-    # Run windeployqt immediately after build
     add_custom_command(TARGET ${target} POST_BUILD
         COMMAND "${CMAKE_COMMAND}" -E
             env PATH="${_qt_bin_dir}" "${WINDEPLOYQT_EXECUTABLE}"
                 --verbose 0
                 --qmldir \"${CMAKE_CURRENT_SOURCE_DIR}\"
-                --no-compiler-runtime
                 \"$<TARGET_FILE:${target}>\"
         COMMENT "Deploying Qt..."
     )
 
-    # windeployqt doesn't work correctly with the system runtime libraries,
-    # so we fall back to one of CMake's own modules for copying them over
-
-    # Doing this with MSVC 2015 requires CMake 3.6+
-    if((MSVC_VERSION VERSION_EQUAL 1900 OR MSVC_VERSION VERSION_GREATER 1900)
-            AND CMAKE_VERSION VERSION_LESS "3.6")
-        message(WARNING "Deploying with MSVC 2015+ requires CMake 3.6+")
-    endif()
     set(CMAKE_INSTALL_UCRT_LIBRARIES TRUE)
     include(InstallRequiredSystemLibraries)
     foreach(lib ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS})
@@ -70,10 +55,9 @@ function(windeployqt target)
             COMMENT "Copying ${filename}..."
         )
     endforeach()
+
 endfunction()
 
-# Add commands that copy the required Qt files to the application bundle
-# represented by the target.
 function(macdeployqt target)
     add_custom_command(TARGET ${target} POST_BUILD
         COMMAND "${MACDEPLOYQT_EXECUTABLE}"
